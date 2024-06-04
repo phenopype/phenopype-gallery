db6a73ab24c65d958ef491d969ca6ca661304c32
mluerig <moritz.luerig@gmail.com>
2024-06-04 19:04:57 -0400

    rm window loc

--- docs_source/_assets/js/custom.js
+++ docs_source/_assets/js/custom.js
@@ -1,13 +1,4 @@
 
-if (window.location.pathname.endsWith('/') === false) {
-	var url = window.location.protocol + '//' + 
-			window.location.host + 
-			window.location.pathname + '/' + 
-			window.location.search;
-	window.location.reload();
-	// window.history.replaceState(null, document.title, url);
-}
-
  function download_file(filelink) {
 	 
 	 	var parts = filelink.split('/');
